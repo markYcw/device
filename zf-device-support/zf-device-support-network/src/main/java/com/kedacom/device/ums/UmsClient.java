5b514c697aa4a5a9175d6941ce8cf549e2932168
shuchuanjun <shuchuanjun@kedacom.com>
2021-05-13 16:00:19 +0800

    feat:提交请求响应

--- zf-device-support/zf-device-support-network/src/main/java/com/kedacom/device/ums/UmsClient.java
+++ zf-device-support/zf-device-support-network/src/main/java/com/kedacom/device/ums/UmsClient.java
@@ -1,10 +1,20 @@
 package com.kedacom.device.ums;
 
+import com.kedacom.device.ums.request.LoginRequest;
+import com.kedacom.device.ums.response.LoginResponse;
+
 /**
  * @author van.shu
  * @create 2021/5/13 14:14
  */
 public interface UmsClient {
+
+    /**
+     * 登录
+     * @param request 请求
+     * @return 响应
+     */
+    LoginResponse login(LoginRequest request);
 
 
 

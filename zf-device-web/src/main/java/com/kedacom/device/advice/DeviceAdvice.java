09b05739f634e8d1d6acf8966df5445cbb069065
hexijian <hexijian@kedacom.com>
2021-05-14 10:39:59 +0800

    修改拼控服务异常捕获

--- zf-device-web/src/main/java/com/kedacom/device/advice/DeviceAdvice.java
+++ zf-device-web/src/main/java/com/kedacom/device/advice/DeviceAdvice.java
@@ -4,10 +4,7 @@ import com.kedacom.BaseResult;
 import com.kedacom.device.common.exception.AuthException;
 import com.kedacom.device.common.exception.ParamException;
 import com.kedacom.device.core.data.DeviceErrorEnum;
-import com.kedacom.device.core.exception.DecoderException;
-import com.kedacom.device.core.exception.MspRemoteCallException;
-import com.kedacom.device.core.exception.TvPlayException;
-import com.kedacom.device.core.exception.TvWallException;
+import com.kedacom.device.core.exception.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +18,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class DeviceAdvice {
+
+    /**
+     * 参数异常捕获
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler({ParamException.class})
+    public BaseResult handleException(ParamException e) {
+        return BaseResult.failed(DeviceErrorEnum.PARAM_ERROR.getCode(), e.getMessage());
+    }
+
+    /**
+     * 远程调用接口异常捕获
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler({MspRemoteCallException.class})
+    public BaseResult handleException(MspRemoteCallException e) {
+        return BaseResult.failed(DeviceErrorEnum.MSP_REMOTE_ERROR.getCode(), DeviceErrorEnum.MSP_REMOTE_ERROR.getMsg());
+    }
 
     /**
      * 鉴权异常捕获
@@ -47,7 +68,7 @@ public class DeviceAdvice {
     }
 
     /**
-     * 大屏异常捕获
+     * 窗口浏览异常捕获
      *
      * @param e
      * @return
@@ -59,7 +80,19 @@ public class DeviceAdvice {
     }
 
     /**
-     * 大屏异常捕获
+     * 预案布局异常捕获
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler({SchemeException.class})
+    public BaseResult handleException(SchemeException e) {
+        return BaseResult.failed(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 解码通道异常捕获
      *
      * @param e
      * @return
@@ -68,24 +101,6 @@ public class DeviceAdvice {
     @ExceptionHandler({DecoderException.class})
     public BaseResult handleException(DecoderException e) {
         return BaseResult.failed(e.getCode(), e.getMessage());
-    }
-
-    /**
-     * 远程调用接口异常捕获
-     *
-     * @param e
-     * @return
-     */
-    @ResponseStatus(HttpStatus.OK)
-    @ExceptionHandler({MspRemoteCallException.class})
-    public BaseResult handleException(MspRemoteCallException e) {
-        return BaseResult.failed(DeviceErrorEnum.MSP_REMOTE_ERROR.getCode(), DeviceErrorEnum.MSP_REMOTE_ERROR.getMsg());
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @ExceptionHandler({ParamException.class})
-    public BaseResult handleException(ParamException e) {
-        return BaseResult.failed(DeviceErrorEnum.PARAM_ERROR.getCode(), e.getMessage());
     }
 
 }

ab4e526bfa3c41967b17a71e286ecef3aa7bf38c
hexijian <hexijian@kedacom.com>
2021-05-14 09:51:26 +0800

    添加停止音频混音的设备参数

---
+++ zf-device-support/zf-device-support-network/src/main/java/com/kedacom/device/stream/request/StopAudioMixDTO.java
@@ -0,0 +1,32 @@
+package com.kedacom.device.stream.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @Auther: hxj
+ * @Date: 2021/5/13 16:44
+ */
+@ToString(callSuper = true)
+@Data
+@ApiModel("停止音频混音业务交互参数")
+public class StopAudioMixDTO extends StreamMediaDTO {
+
+    private static final String COMMAND = "stopaudiomix";
+
+    @ApiModelProperty("音频混音设备组id")
+    private String GroupID;
+
+    @ApiModelProperty("画面合成ID集合")
+    private List<String> mixIDs;
+
+    @Override
+    public String getCommand() {
+        return COMMAND;
+    }
+
+}

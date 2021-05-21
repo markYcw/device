0ccb7d3141502c4c74e2e0e5961aa4ea143de4e0
hexijian <hexijian@kedacom.com>
2021-05-21 14:00:41 +0800

    add: 添加流媒体feign接口

--- zf-device-api/src/main/java/com/kedacom/device/api/streammedia/StreamMediaApi.java
+++ zf-device-api/src/main/java/com/kedacom/device/api/streammedia/StreamMediaApi.java
@@ -1,7 +1,14 @@
 package com.kedacom.device.api.streammedia;
 
+import com.kedacom.BaseResult;
 import com.kedacom.device.api.streammedia.fallback.StreamMediaApiFallbackFactory;
+import com.kedacom.streamMedia.request.*;
+import com.kedacom.streamMedia.response.*;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /**
  * @Auther: hxj
@@ -9,4 +16,147 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient(contextId = "streamMediaApi", path = "device/streamMedia", fallbackFactory = StreamMediaApiFallbackFactory.class)
 public interface StreamMediaApi {
+
+
+    /**
+     * 开启录像
+     *
+     * @param startrecDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/startRec")
+    BaseResult<StartRecResponseVO> startRec(@RequestBody StartRecDTO startrecDTO);
+
+    /**
+     * 停止录像
+     *
+     * @param stoprecDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/stopRec")
+    BaseResult<Boolean> stopRec(@RequestBody StopRecDTO stoprecDTO);
+
+    /**
+     * 查询录像记录
+     *
+     * @param queryrecDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/queryRec")
+    BaseResult<QueryRecResponseVO> queryRec(@RequestBody QueryRecDTO queryrecDTO);
+
+    /**
+     * 开启音频混音:返回混音设备分组id、混音ID
+     *
+     * @param startAudioMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/startAudioMix")
+    BaseResult<StartAudioMixResponseVO> startAudioMix(@RequestBody StartAudioMixDTO startAudioMixDTO);
+
+    /**
+     * 停止音频混音
+     *
+     * @param stopAudioMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/stopAudioMix")
+    BaseResult<Boolean> stopAudioMix(@RequestBody StopAudioMixDTO stopAudioMixDTO);
+
+    /**
+     * 更新音频混音
+     *
+     * @param updateAudioMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/updateAudioMix")
+    BaseResult<Boolean> updateAudioMix(@RequestBody UpdateAudioMixDTO updateAudioMixDTO);
+
+
+    /**
+     * 查询所有混音 返回混音ID集合
+     *
+     * @param queryAllAudioMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/queryAllAudioMix")
+    BaseResult<List<String>> queryAllAudioMix(@RequestBody QueryAllAudioMixDTO queryAllAudioMixDTO);
+
+    /**
+     * 查询混音信息
+     *
+     * @param queryAudioMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/queryAudioMix")
+    BaseResult<QueryAudioMixResponseVO> queryAudioMix(@RequestBody QueryAudioMixDTO queryAudioMixDTO);
+
+    /**
+     * 开始画面合成: 返回画面合成设备分组id、画面合成ID
+     *
+     * @param startVideoMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/startVideoMix")
+    BaseResult<StartVideoMixResponseVO> startVideoMix(@RequestBody StartVideoMixDTO startVideoMixDTO);
+
+    /**
+     * 停止画面合成
+     *
+     * @param stopVideoMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/stopVideoMix")
+    BaseResult<Boolean> stopVideoMix(@RequestBody StopVideoMixDTO stopVideoMixDTO);
+
+    /**
+     * 更新画面合成
+     *
+     * @param updateVideoMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/updateVideoMix")
+    BaseResult<Boolean> updateVideoMix(@RequestBody UpdateVideoMixDTO updateVideoMixDTO);
+
+    /**
+     * 查询所有画面合成:返回mixIDs 合成ID集合
+     *
+     * @param queryAllVideoMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/queryAllVideoMix")
+    BaseResult<QueryAllAudioMixVO> queryAllVideoMix(@RequestBody QueryAllVideoMixDTO queryAllVideoMixDTO);
+
+    /**
+     * 查询画面合成信息
+     *
+     * @param queryVideoMixDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/queryVideoMix")
+    BaseResult<QueryVideoMixResponseVO> queryVideoMix(@RequestBody QueryVideoMixDTO queryVideoMixDTO);
+
+    /**
+     * 发送透明通道数据
+     *
+     * @param sendTransDataDTO
+     * @param br
+     * @return
+     */
+    @PostMapping("/sendTransData")
+    BaseResult<Boolean> sendTransData(@RequestBody SendTransDataDTO sendTransDataDTO);
+
 }
--- zf-device-api/src/main/java/com/kedacom/device/api/streammedia/fallback/StreamMediaApiFallbackFactory.java
+++ zf-device-api/src/main/java/com/kedacom/device/api/streammedia/fallback/StreamMediaApiFallbackFactory.java
@@ -1,7 +1,12 @@
 package com.kedacom.device.api.streammedia.fallback;
 
+import com.kedacom.BaseResult;
 import com.kedacom.device.api.streammedia.StreamMediaApi;
+import com.kedacom.streamMedia.request.*;
+import com.kedacom.streamMedia.response.*;
 import feign.hystrix.FallbackFactory;
+
+import java.util.List;
 
 /**
  * @Auther: hxj
@@ -11,6 +16,75 @@ public class StreamMediaApiFallbackFactory implements FallbackFactory<StreamMedi
     @Override
     public StreamMediaApi create(Throwable throwable) {
         return new StreamMediaApi() {
+            @Override
+            public BaseResult<StartRecResponseVO> startRec(StartRecDTO startrecDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> stopRec(StopRecDTO stoprecDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<QueryRecResponseVO> queryRec(QueryRecDTO queryrecDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<StartAudioMixResponseVO> startAudioMix(StartAudioMixDTO startAudioMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> stopAudioMix(StopAudioMixDTO stopAudioMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> updateAudioMix(UpdateAudioMixDTO updateAudioMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<List<String>> queryAllAudioMix(QueryAllAudioMixDTO queryAllAudioMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<QueryAudioMixResponseVO> queryAudioMix(QueryAudioMixDTO queryAudioMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<StartVideoMixResponseVO> startVideoMix(StartVideoMixDTO startVideoMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> stopVideoMix(StopVideoMixDTO stopVideoMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> updateVideoMix(UpdateVideoMixDTO updateVideoMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<QueryAllAudioMixVO> queryAllVideoMix(QueryAllVideoMixDTO queryAllVideoMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<QueryVideoMixResponseVO> queryVideoMix(QueryVideoMixDTO queryVideoMixDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
+
+            @Override
+            public BaseResult<Boolean> sendTransData(SendTransDataDTO sendTransDataDTO) {
+                return BaseResult.failed("服务出错，请稍后重试");
+            }
         };
     }
 }

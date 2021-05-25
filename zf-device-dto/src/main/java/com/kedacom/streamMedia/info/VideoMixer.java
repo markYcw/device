f77996e9ae6288aca04e5aa6ed4667c77794b8e3
hexijian <hexijian@kedacom.com>
2021-05-25 16:27:44 +0800

    modify: 修改新媒体IP、端口

--- zf-device-dto/src/main/java/com/kedacom/streamMedia/info/VideoMixer.java
+++ zf-device-dto/src/main/java/com/kedacom/streamMedia/info/VideoMixer.java
@@ -20,11 +20,11 @@ public class VideoMixer implements Serializable {
     @ApiModelProperty(value = "画面合成通道索引，从0开始",required = true)
     private Integer window_index;
 
-    @NotBlank(message = "参与混音方终端ID不能为空")
-    @ApiModelProperty(value = "参与混音方终端ID",required = true)
+    @NotBlank(message = "设备国标id不能为空")
+    @ApiModelProperty(value = "设备国标id",required = true)
     private String resourceID;
 
-    @ApiModelProperty("域ID。")
+    @ApiModelProperty("域ID")
     private Integer nmedia_id;
 
     @ApiModelProperty("显示背景图。默认为 0-不显示；1、会议终端；2、执法记录仪；3、单兵；4、IPC；5、人像卡口；6、视信通；7、手机；8、固话；9、350M；10、通用类型；99、自定义背景图片地址（通过 pic_url 传入）。1080p、layout=1 时，图片大小为960*540，居中显示，会根据 layout等比例缩放")

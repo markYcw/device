2d890a7cc82e7bef789af65d97e12331ecc7e8b0
hexijian <hexijian@kedacom.com>
2021-06-02 10:31:24 +0800

    modify:  修改设备状态变更通知类

--- zf-device-support/zf-device-support-network/src/main/java/com/kedacom/core/AcceptUrlListen.java
+++ zf-device-support/zf-device-support-network/src/main/java/com/kedacom/core/AcceptUrlListen.java
@@ -12,10 +12,10 @@ import java.io.Serializable;
 @Data
 public class AcceptUrlListen implements Serializable {
 
-    @ApiModelProperty("主题")
+    @ApiModelProperty("主题统一为:\"zf_ums_topic_AV_DEVICE_STATUS\"")
     private String topic;
 
-    @ApiModelProperty("接受消息URL")
+    @ApiModelProperty("接受消息URL(即接受通知消息的接口地址):例子-->url:http://127.0.0.1:10090/demo/deviceStatus    @RequestBody:UmsSubDeviceStatusModel model")
     private String acceptUrl;
 
 }

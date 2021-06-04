5ccc427e077fcd8dcc30b0543d11fb5b0c7ff78f
hexijian <hexijian@kedacom.com>
2021-06-04 09:18:29 +0800

    modify:  修改一些代码注释

--- zf-device-dto/src/main/java/com/kedacom/streamMedia/request/SendTransDataDTO.java
+++ zf-device-dto/src/main/java/com/kedacom/streamMedia/request/SendTransDataDTO.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 public class SendTransDataDTO implements Serializable {
 
     @NotBlank(message = "平台id不能为空")
-    @ApiModelProperty(value = "平台id",required = true)
+    @ApiModelProperty(value = "平台id", required = true)
     private String umsId;
 
     @NotBlank(message = "设备国标id不能为空")
@@ -25,14 +25,14 @@ public class SendTransDataDTO implements Serializable {
     private String DeviceID;
 
     @NotNull(message = "透明数据扩展不能为空")
-    @ApiModelProperty(value = "是否是透明数据扩展,默认值:0,0:不是、1:是", required = true)
+    @ApiModelProperty(value = "是否是透明数据扩展,默认值:0,0:不是、1:是;填默认值就可以", required = true)
     private Integer Ext;
 
     @ApiModelProperty(value = "控制类型。当Ext为1时AppCmd不起作用")
     private Integer AppCmd;
 
     @NotBlank(message = "透明数据不能为空")
-    @ApiModelProperty(value = "透明数据", required = true)
+    @ApiModelProperty(value = "透明数据;业务方定义-->{svr配置1是走路、TransData发送个1就可以控制他走路}", required = true)
     private String TransData;
 
 }
--- zf-device-dto/src/main/java/com/kedacom/ums/entity/AcceptUrlListen.java
+++ zf-device-dto/src/main/java/com/kedacom/ums/entity/AcceptUrlListen.java
@@ -15,7 +15,7 @@ public class AcceptUrlListen implements Serializable {
     @ApiModelProperty("主题统一为:\"zf_ums_topic_AV_DEVICE_STATUS\"")
     private String topic;
 
-    @ApiModelProperty("接受消息URL(即接受通知消息的接口地址):例子-->url:http://127.0.0.1:10090/demo/deviceStatus    @RequestBody:UmsSubDeviceStatusModel model")
+    @ApiModelProperty("接受设备状态变更通知消息的接口地址如:http://127.0.0.1:10090/demo/deviceStatus;调用方接口定义如下要求-->{@PostMapping、@RequestBody UmsSubDeviceStatusModel model}")
     private String acceptUrl;
 
 }

c8db919d57aa569313c4d13fe81c8b8b8fbac0b8
wangxuyang <wangxuyang@kedacom.com>
2021-06-19 15:32:56 +0800

    配置多套平台信息，并对应相关的设备（）字段转换错误

--- zf-device-core/src/main/java/com/kedacom/device/core/convert/UmsSubDeviceConvert.java
+++ zf-device-core/src/main/java/com/kedacom/device/core/convert/UmsSubDeviceConvert.java
@@ -22,7 +22,7 @@ public interface UmsSubDeviceConvert {
 
     UmsSubDeviceConvert INSTANCE = Mappers.getMapper(UmsSubDeviceConvert.class);
 
-    @Mappings({@Mapping(source = "ip", target = "deviceId"),
+    @Mappings({@Mapping(source = "id", target = "deviceId"),
             @Mapping(source = "ipv4", target = "deviceIp"),
             @Mapping(source = "manufactorCode_name",target = "manufactorName"),
             @Mapping(source = "status",target = "deviceStatus"),

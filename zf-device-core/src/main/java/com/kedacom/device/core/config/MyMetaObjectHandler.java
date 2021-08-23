4d22e31c8edbfce8859272f3cd99bc727eef9804
hexijian <hexijian@kedacom.com>
2021-08-23 10:42:58 +0800

    modify: 配置mybatisplus的自动填充

--- zf-device-core/src/main/java/com/kedacom/device/core/config/MyMetaObjectHandler.java
+++ zf-device-core/src/main/java/com/kedacom/device/core/config/MyMetaObjectHandler.java
@@ -1,6 +1,5 @@
 package com.kedacom.device.core.config;
 
-import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
@@ -15,16 +14,16 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTime", Date.class, DateUtil.now());
-        this.strictInsertFill(metaObject, "modifyTime", Date.class, DateUtil.now());
-        this.strictInsertFill(metaObject, "updateTime", Date.class, DateUtil.now());
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "modifyTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "modifyTime", Date.class, DateUtil.now());
-        this.strictUpdateFill(metaObject, "updateTime", Date.class, DateUtil.now());
+        this.strictUpdateFill(metaObject, "modifyTime", Date.class, new Date());
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
     }
 
 }

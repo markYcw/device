0cb2f338d01432a49c05b207ba07fa0a3ac1d1cb
hexijian <hexijian@kedacom.com>
2021-08-23 09:42:47 +0800

    modify: mcu的feign接口

--- zf-device-api/src/main/java/com/kedacom/device/api/mcu/McuApi.java
+++ zf-device-api/src/main/java/com/kedacom/device/api/mcu/McuApi.java
@@ -1,13 +1,120 @@
 package com.kedacom.device.api.mcu;
 
+import com.kedacom.BaseResult;
 import com.kedacom.device.api.mcu.fallback.McuApiFallbackFactory;
+import com.kedacom.mp.mcu.McuRequestDTO;
+import com.kedacom.mp.mcu.request.*;
+import com.kedacom.mp.mcu.response.*;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @Author hxj
  * @Date: 2021/8/12 13:37
  * @Description 会议平台qpi
  */
-@FeignClient(name = "device-server",contextId = "meetingPlatformApi",path = "/api-device/ums/mcu",fallbackFactory = McuApiFallbackFactory.class)
+@FeignClient(name = "device-server", contextId = "mcuApi", path = "/api-device/ums/mcu", fallbackFactory = McuApiFallbackFactory.class)
 public interface McuApi {
+
+    @ApiOperation("登录平台")
+    @PostMapping("/login")
+    BaseResult<McuLoginVO> login(@RequestBody McuRequestDTO dto);
+
+    @ApiOperation("登出平台")
+    @PostMapping("/logout")
+    BaseResult logout(@RequestBody McuRequestDTO dto);
+
+    @ApiOperation("创建/删除账户")
+    @PostMapping("/account")
+    BaseResult account(@RequestBody McuAccountDTO dto);
+
+    @ApiOperation("获取会议列表")
+    @PostMapping("/confs")
+    BaseResult<McuConfsVO> confs(@RequestBody McuConfsDTO dto);
+
+    @ApiOperation("获取会议模板列表")
+    @PostMapping("/templates")
+    BaseResult<McuConfsVO> templates(@RequestBody McuTemplatesDTO dto);
+
+    @ApiOperation("获取会议信息")
+    @PostMapping("/confinfo")
+    BaseResult<McuConfInfoVO> confinfo(@RequestBody McuConfInfoDTO dto);
+
+    @ApiOperation("创建/删除会议")
+    @PostMapping("/conf")
+    BaseResult<McuConfVO> conf(@RequestBody McuConfDTO dto);
+
+    @ApiOperation("开启会议模板")
+    @PostMapping("/confTemplate")
+    BaseResult confTemplate(@RequestBody McuConfTemplateDTO dto);
+
+    @ApiOperation("获取与会成员")
+    @PostMapping("/mtMembers")
+    BaseResult<McuMtmembersVO> mtMembers(@RequestBody McuMtMembersDTO dto);
+
+    @ApiOperation("添加/删除终端")
+    @PostMapping("/mt")
+    BaseResult<McuMtVO> mt(@RequestBody McuMtDTO dto);
+
+    @ApiOperation("呼叫/挂断终端")
+    @PostMapping("/mtCall")
+    BaseResult mtCall(@RequestBody McuMtCallDTO dto);
+
+    @ApiOperation("设置/取消发言人")
+    @PostMapping("/speaker")
+    BaseResult speaker(@RequestBody McuSpeakerDTO dto);
+
+    @ApiOperation("设置/取消主席")
+    @PostMapping("/chairman")
+    BaseResult chairman(@RequestBody McuChairmanDTO dto);
+
+    @ApiOperation("静音")
+    @PostMapping("/silence")
+    BaseResult silence(@RequestBody McuSilenceDTO dto);
+
+    @ApiOperation("哑音")
+    @PostMapping("/mute")
+    BaseResult mute(@RequestBody McuMuteDTO dto);
+
+    @ApiOperation("调节音量")
+    @PostMapping("/volume")
+    BaseResult volume(@RequestBody McuVolumeDTO dto);
+
+    @ApiOperation("终端双流控制")
+    @PostMapping("/dual")
+    BaseResult dual(@RequestBody McuDualDTO dto);
+
+    @ApiOperation("开始/停止画面合成")
+    @PostMapping("/videoMix")
+    BaseResult videoMix(@RequestBody McuVideoMixDTO dto);
+
+    @ApiOperation("开始/停止混音")
+    @PostMapping("/audioMix")
+    BaseResult audioMix(@RequestBody McuAudioMixDTO dto);
+
+    @ApiOperation("添加/删除混音成员")
+    @PostMapping("/audioMixMember")
+    BaseResult<McuAudioMixMemberVO> audioMixMember(@RequestBody McuAudioMixMemberDTO dto);
+
+    @ApiOperation("开始/暂停/恢复/停止录像")
+    @PostMapping("/rec")
+    public BaseResult rec(@RequestBody McuRecDTO dto);
+
+    @ApiOperation("获取电视墙列表")
+    @PostMapping("/tvWalls")
+    public BaseResult<McuTvWallsVO> tvWalls(@RequestBody McuRequestDTO dto);
+
+    @ApiOperation("开始/停止上电视墙")
+    @PostMapping("/tvwall")
+    BaseResult tvwall(@RequestBody McuTvWallDTO dto);
+
+    @ApiOperation("开始/停止码流交换")
+    @PostMapping("/exchange")
+    BaseResult exchange(@RequestBody McuExchangeDTO dto);
+
+    @ApiOperation("发送短消息")
+    @PostMapping("/message")
+    BaseResult message(@RequestBody McuMessageDTO dto);
 }
--- zf-device-api/src/main/java/com/kedacom/device/api/mcu/fallback/McuApiFallbackFactory.java
+++ zf-device-api/src/main/java/com/kedacom/device/api/mcu/fallback/McuApiFallbackFactory.java
@@ -1,6 +1,10 @@
 package com.kedacom.device.api.mcu.fallback;
 
+import com.kedacom.BaseResult;
 import com.kedacom.device.api.mcu.McuApi;
+import com.kedacom.mp.mcu.McuRequestDTO;
+import com.kedacom.mp.mcu.request.*;
+import com.kedacom.mp.mcu.response.*;
 import feign.hystrix.FallbackFactory;
 
 /**
@@ -10,7 +14,132 @@ import feign.hystrix.FallbackFactory;
  */
 public class McuApiFallbackFactory implements FallbackFactory<McuApi> {
     @Override
-    public McuApi create(Throwable cause) {
-        return null;
+    public McuApi create(Throwable throwable) {
+        return new McuApi() {
+            @Override
+            public BaseResult<McuLoginVO> login(McuRequestDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult logout(McuRequestDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult account(McuAccountDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuConfsVO> confs(McuConfsDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuConfsVO> templates(McuTemplatesDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuConfInfoVO> confinfo(McuConfInfoDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuConfVO> conf(McuConfDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult confTemplate(McuConfTemplateDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuMtmembersVO> mtMembers(McuMtMembersDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuMtVO> mt(McuMtDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult mtCall(McuMtCallDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult speaker(McuSpeakerDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult chairman(McuChairmanDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult silence(McuSilenceDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult mute(McuMuteDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult volume(McuVolumeDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult dual(McuDualDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult videoMix(McuVideoMixDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult audioMix(McuAudioMixDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuAudioMixMemberVO> audioMixMember(McuAudioMixMemberDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult rec(McuRecDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult<McuTvWallsVO> tvWalls(McuRequestDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult tvwall(McuTvWallDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult exchange(McuExchangeDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+
+            @Override
+            public BaseResult message(McuMessageDTO dto) {
+                return BaseResult.failed(throwable.getMessage());
+            }
+        };
     }
 }

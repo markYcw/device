0463be074ab568e10e457cf17a315c9dd312e087
yucongwang <yucongwang@kedacom.com>
2021-09-24 13:12:58 +0800

    优化

--- zf-device-dto/src/main/java/com/kedacom/mp/mcu/request/McuDualDTO.java
+++ zf-device-dto/src/main/java/com/kedacom/mp/mcu/request/McuDualDTO.java
@@ -25,8 +25,7 @@ public class McuDualDTO extends McuRequestDTO {
     @ApiModelProperty(value = "会议号码", required = true)
     private String confId;
 
-    @NotBlank(message = "终端id不能为空")
-    @ApiModelProperty(value = "终端id", required = true)
+    @ApiModelProperty(value = "终端id")
     private String mtId;
 
 }

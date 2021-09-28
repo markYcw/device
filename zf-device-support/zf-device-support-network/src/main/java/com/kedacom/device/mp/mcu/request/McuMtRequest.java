36d59da4ec47af5a65aa64014929696375116705
yucongwang <yucongwang@kedacom.com>
2021-09-28 14:29:49 +0800

    更改添加终端接口

--- zf-device-support/zf-device-support-network/src/main/java/com/kedacom/device/mp/mcu/request/McuMtRequest.java
+++ zf-device-support/zf-device-support-network/src/main/java/com/kedacom/device/mp/mcu/request/McuMtRequest.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -15,31 +17,27 @@ import java.io.Serializable;
 @ApiModel(description =  "添加/删除终端向中间件请求参数")
 public class McuMtRequest implements Serializable {
 
+    @NotNull(message = "类型不能为空")
     @ApiModelProperty(value = "0：添加终端;1：删除终端",required = true)
     private Integer type;
 
+    @NotBlank(message = "会议号码不能为空")
     @ApiModelProperty(value = "会议号码",required = true)
     private String confId;
 
-    @ApiModelProperty(value = "终端id")
+    @ApiModelProperty(value = "终端ID，删除时必填")
     private String mtId;
 
-    @ApiModelProperty(value = "终端号")
-    private String mtAccount;
+    @ApiModelProperty(value = "终端E164号,IP或电话号码 添加时必填")
+    private String account;
 
-    @ApiModelProperty(value = "终端类型\n" +
-            "5-e164号码；\n" +
-            "6-电话；\n" +
-            "7-ip地址；\n" +
-            "8-别名@ip(监控前端)；")
-    private Integer mtType;
+    @ApiModelProperty(value = "终端类型 5-e164号码；6-电话；7-ip地址；8-别名@ip(监控前端)； 添加时必填")
+    private Integer accountType;
 
     @ApiModelProperty(value = "呼叫码率，添加时必填")
     private Integer bitrate;
 
-    @ApiModelProperty(value = "呼叫协议\n" +
-            "0-H323；\n" +
-            "1-SIP；")
+    @ApiModelProperty(value = "呼叫协议 0-H323；1-SIP； 添加时必填")
     private Integer protocol;
 
 }

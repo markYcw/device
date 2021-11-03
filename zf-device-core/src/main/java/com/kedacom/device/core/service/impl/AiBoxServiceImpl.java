ea026ae88d4f10aa6de19bae3613533131747470
wangxuyang <wangxuyang@kedacom.com>
2021-11-03 12:14:50 +0800

    aibox增加最大最小阈值字段

--- zf-device-core/src/main/java/com/kedacom/device/core/service/impl/AiBoxServiceImpl.java
+++ zf-device-core/src/main/java/com/kedacom/device/core/service/impl/AiBoxServiceImpl.java
@@ -28,10 +28,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;

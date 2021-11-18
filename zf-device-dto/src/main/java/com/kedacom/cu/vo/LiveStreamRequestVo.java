d51ed010b8092e15629548e6086759a394d4b9d3
wangxuyang <wangxuyang@kedacom.com>
2021-11-18 11:16:01 +0800

    发布直播点播请求参数是否必填

--- zf-device-dto/src/main/java/com/kedacom/cu/vo/LiveStreamRequestVo.java
+++ zf-device-dto/src/main/java/com/kedacom/cu/vo/LiveStreamRequestVo.java
@@ -21,47 +21,45 @@ public class LiveStreamRequestVo implements Serializable {
     private String id;
 
     @NotNull
-    @ApiModelProperty("发布类型, 1：rtsp 2: rtmp 3: webrtc 4: httpflv(http) 5: httpflv-ws(websocket)")
+    @ApiModelProperty(value = "发布类型, 1：rtsp 2: rtmp 3: webrtc 4: httpflv(http) 5: httpflv-ws(websocket)", required = true)
     private Integer type;
 
     @NotNull
-    @ApiModelProperty("设备类型, 0：1.0监控平台; 2：3带高清终端; 4：5.0会议平台; 11：5.0终端")
+    @ApiModelProperty(value = "设备类型, 0：1.0监控平台; 2：3带高清终端; 4：5.0会议平台; 11：5.0终端", required = true)
     private Integer devtype;
 
     @NotNull
-    @ApiModelProperty("校验媒体类型, 0:校验音频和视频; 1:只校验视频")
+    @ApiModelProperty(value = "校验媒体类型, 0:校验音频和视频; 1:只校验视频", required = true)
     private Integer checkmediatype;
 
     @NotBlank
-    @ApiModelProperty("ip地址")
+    @ApiModelProperty(value = "ip地址", required = true)
     private String ip;
 
     @NotNull
-    @ApiModelProperty("端口")
+    @ApiModelProperty(value = "端口", required = true)
     private Integer port;
 
     @NotBlank
-    @ApiModelProperty("用户名")
+    @ApiModelProperty(value = "登录账户", required = true)
     private String user;
 
     @NotBlank
-    @ApiModelProperty("密码")
+    @ApiModelProperty(value = "密码", required = true)
     private String pwd;
 
     @NotBlank
-    @ApiModelProperty("监控平台：PUID号 :  会议平台：会议号")
+    @ApiModelProperty(value = "监控平台：PUID号 :  会议平台：会议号", required = true)
     private String src;
 
     @NotBlank
-    @ApiModelProperty("1 监控平台：通道号 2 会议终端：码流类型 3 会议平台：与会终端")
+    @ApiModelProperty(value = "1 监控平台：通道号 2 会议终端：码流类型 3 会议平台：与会终端", required = true)
     private String chn;
 
-    @NotBlank
     @ApiModelProperty("5.0以上会议平台用，其它可以不填写")
     private String key;
 
-    @NotBlank
-    @ApiModelProperty("5.0以上会议平台用，其它可以不填写")
+    @ApiModelProperty(value = "5.0以上会议平台用，其它可以不填写")
     private String secret;
 
     @ApiModelProperty("组信息")
--- zf-device-dto/src/main/java/com/kedacom/cu/vo/VodStreamRequestVo.java
+++ zf-device-dto/src/main/java/com/kedacom/cu/vo/VodStreamRequestVo.java
@@ -21,51 +21,51 @@ public class VodStreamRequestVo implements Serializable {
     private String id;
 
     @NotNull
-    @ApiModelProperty("发布类型, 1：rtsp，2: rtmp，3: webrtc")
+    @ApiModelProperty(value = "发布类型, 1：rtsp，2: rtmp，3: webrtc", required = true)
     private Integer type;
 
     @NotNull
-    @ApiModelProperty("设备类型, 0：1.0监控平台， 2：3带高清终端， 4：5.0会议平台， 11：5.0终端")
+    @ApiModelProperty(value = "设备类型, 0：1.0监控平台， 2：3带高清终端， 4：5.0会议平台， 11：5.0终端", required = true)
     private Integer devtype;
 
     @NotNull
-    @ApiModelProperty("校验媒体类型, 0:校验音频和视频：1:只校验视频")
+    @ApiModelProperty(value = "校验媒体类型, 0:校验音频和视频：1:只校验视频", required = true)
     private Integer checkmediatype;
 
     @NotBlank
-    @ApiModelProperty("监控平台id")
+    @ApiModelProperty(value = "平台id", required = true)
     private String ip;
 
     @NotNull
-    @ApiModelProperty("监控平台端口")
+    @ApiModelProperty(value = "平台端口", required = true)
     private Integer port;
 
     @NotBlank
-    @ApiModelProperty("监控平台登录用户")
+    @ApiModelProperty(value = "平台登录用户", required = true)
     private String user;
 
     @NotBlank
-    @ApiModelProperty("监控平台登录密码")
+    @ApiModelProperty(value = "平台登录密码", required = true)
     private String pwd;
 
     @NotBlank
-    @ApiModelProperty("监控平台：PUID号，会议平台：会议号")
+    @ApiModelProperty(value = "监控平台：PUID号，会议平台：会议号", required = true)
     private String src;
 
     @NotBlank
-    @ApiModelProperty("监控平台：通道号，会议终端：码流类型，会议平台：与会终端")
+    @ApiModelProperty(value = "监控平台：通道号，会议终端：码流类型，会议平台：与会终端", required = true)
     private String chn;
 
     @NotBlank
-    @ApiModelProperty("开始时间 如：20201116080000")
+    @ApiModelProperty(value = "开始时间 如：20201116080000", required = true)
     private String start;
 
     @NotBlank
-    @ApiModelProperty("结束时间 如：20201117140000")
+    @ApiModelProperty(value = "结束时间 如：20201117140000", required = true)
     private String end;
 
     @NotNull
-    @ApiModelProperty("录像来源（监控平台有用）：1：当前平台， 2：设备所在平台，3：前端")
+    @ApiModelProperty(value = "录像来源（监控平台有用）：1：当前平台， 2：设备所在平台，3：前端", required = true)
     private Integer recsrc;
 
     @ApiModelProperty("用户填写的唯一标识")

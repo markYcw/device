764cb9f02e5c8e251e5565b594c054ec69d7c1af
yucongwang <yucongwang@kedacom.com>
2021-12-17 15:58:56 +0800

    缩小restTemplate写响应超时时间

--- zf-device-core/src/main/java/com/kedacom/device/core/utils/RemoteRestTemplate.java
+++ zf-device-core/src/main/java/com/kedacom/device/core/utils/RemoteRestTemplate.java
@@ -31,7 +31,7 @@ public class RemoteRestTemplate {
         OkHttp3ClientHttpRequestFactory httpRequestFactory = new OkHttp3ClientHttpRequestFactory();
         httpRequestFactory.setConnectTimeout(15000);
         httpRequestFactory.setReadTimeout(40000);
-        httpRequestFactory.setWriteTimeout(1000);
+        httpRequestFactory.setWriteTimeout(30000);
 
         restTemplate = new RestTemplate(httpRequestFactory);
         setRestTemplateEncode(restTemplate);

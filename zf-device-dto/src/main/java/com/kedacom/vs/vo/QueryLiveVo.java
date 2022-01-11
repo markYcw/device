2c4bdf5f04463f62bbb816b98d3059ccce6e4c46
yucongwang <yucongwang@kedacom.com>
2022-01-11 16:40:56 +0800

    Mcu新增查询vrs列表接口

--- zf-device-dto/src/main/java/com/kedacom/vs/vo/QueryLiveVo.java
+++ zf-device-dto/src/main/java/com/kedacom/vs/vo/QueryLiveVo.java
@@ -16,6 +16,7 @@ public class QueryLiveVo {
     @ApiModelProperty("查询起始，从0开始")
     private Integer start;
 
+    @ApiModelProperty("查询总数")
     private Integer count;
 
     @ApiModelProperty("模糊匹配的录像名字")

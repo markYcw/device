c60a3dc3f380989439976799d357300d5f3c7813
yucongwang <yucongwang@kedacom.com>
2022-02-15 09:49:20 +0800

    仓门控制注释优化

--- zf-device-dto/src/main/java/com/kedacom/svr/dto/DvdDoorCtrlVo.java
+++ zf-device-dto/src/main/java/com/kedacom/svr/dto/DvdDoorCtrlVo.java
@@ -18,8 +18,8 @@ public class DvdDoorCtrlVo extends SvrRequestDto{
     @ApiModelProperty("0：开仓 1：关仓 2：一键校验（暂不支持） 3：光盘封口（暂不支持）")
     private Integer type;
 
-    @NotNull(message = "仓门ID不能为空")
-    @ApiModelProperty("仓门ID")
+    @NotNull(message = "dvd编号不能为空")
+    @ApiModelProperty("dvd编号")
     private Integer dvdId;
 
 

7a0a23e018e10e6aa2e45dba0f49f2364ff8d466
yucongwang <yucongwang@kedacom.com>
2022-02-23 15:57:59 +0800

    添加编码通道列表去除字段

--- zf-device-dto/src/main/java/com/kedacom/svr/pojo/OnVifInfo.java
+++ zf-device-dto/src/main/java/com/kedacom/svr/pojo/OnVifInfo.java
@@ -14,14 +14,6 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class OnVifInfo {
 
-    @NotBlank(message = "onvif设备uuid不能为空")
-    @ApiModelProperty(value = "onvif设备uuid",required = true)
-    private String uuid;
-
-    @NotBlank(message = "onvif设备xaddr不能为空")
-    @ApiModelProperty(value = "onvif设备xaddr",required = true)
-    private String xaddr;
-
     @NotBlank(message = "onvif设备登录用户名不能为空")
     @ApiModelProperty(value = "onvif设备登录用户名",required = true)
     private String userName;

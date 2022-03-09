b6a0ffc19ef2845a0f4bb3eb2b024e801c1dcc1a
yucongwang <yucongwang@kedacom.com>
2022-03-09 11:07:07 +0800

    SVR设备类型改为后台插入

---
+++ zf-device-web/src/main/resources/db/migration/V1_22__modify.sql
@@ -0,0 +1,13 @@
+
+use bmp_new_udms;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (1, 'SVR2832');
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (2, 'SVR2931');
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (3, 'SVR2830');
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (4, 'SVR2831');
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (5, 'SVR2916');
+INSERT INTO `km_svr_type`(`id`, `type`) VALUES (6, 'SVR2930');
+

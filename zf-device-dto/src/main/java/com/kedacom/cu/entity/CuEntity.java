f00a20269d388154fe7be9e124722bbde69b5292
yucongwang <yucongwang@kedacom.com>
2022-05-30 15:48:05 +0800

    解决jsonfomat报错

--- zf-device-dto/src/main/java/com/kedacom/cu/entity/CuEntity.java
+++ zf-device-dto/src/main/java/com/kedacom/cu/entity/CuEntity.java
@@ -4,9 +4,9 @@ import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -81,6 +81,7 @@ public class CuEntity implements Serializable {
 	 */
 	@ApiModelProperty(value = "创建时间")
 	@TableField(fill = FieldFill.INSERT)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createTime;
 
@@ -89,6 +90,7 @@ public class CuEntity implements Serializable {
 	 */
 	@ApiModelProperty(value = "修改时间")
 	@TableField(fill = FieldFill.INSERT_UPDATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date modifyTime;
 

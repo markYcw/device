3143bce0078121f7bcf4c9b6f928f189b057a21f
“hexijian” <hexijian@kedacom.com>
2022-06-08 11:19:52 +0800

    modify:增加其他版本的SQL

---
+++ zf-device-web/src/main/resources/db/dm/V1_22__modify.sql
@@ -0,0 +1,12 @@
+INSERT INTO km_svr_type(id, type) VALUES (1, 'SVR2832');
+
+INSERT INTO km_svr_type(id, type) VALUES (2, 'SVR2931');
+
+INSERT INTO km_svr_type(id, type) VALUES (3, 'SVR2830');
+
+INSERT INTO km_svr_type(id, type) VALUES (4, 'SVR2831');
+
+INSERT INTO km_svr_type(id, type) VALUES (5, 'SVR2916');
+
+INSERT INTO km_svr_type(id, type) VALUES (6, 'SVR2930');
+

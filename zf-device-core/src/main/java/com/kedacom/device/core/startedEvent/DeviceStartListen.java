7fafd0b9dea0461233f766295bd5d07192309594
“hexijian” <hexijian@kedacom.com>
2022-06-08 11:06:29 +0800

    modify:增加tcp自启动连接、添加电源SQL

--- zf-device-core/src/main/java/com/kedacom/device/core/startedEvent/DeviceStartListen.java
+++ zf-device-core/src/main/java/com/kedacom/device/core/startedEvent/DeviceStartListen.java
@@ -20,7 +20,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @auther hxj
+ * @author hexijian
  * @date 2021/5/20 09:51
  */
 @Component

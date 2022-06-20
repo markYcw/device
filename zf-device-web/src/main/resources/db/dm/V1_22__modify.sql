2a953271541594bef0897b343304937b5d29ddae
yucongwang <yucongwang@kedacom.com>
2022-06-20 11:27:02 +0800

    破坏主键自增

--- zf-device-web/src/main/resources/db/dm/V1_22__modify.sql
+++ zf-device-web/src/main/resources/db/dm/V1_22__modify.sql
@@ -1,3 +1,5 @@
+SET IDENTITY_INSERT km_vrs_name on;
+
 INSERT INTO km_svr_type(id, type) VALUES (1, 'SVR2832');
 
 INSERT INTO km_svr_type(id, type) VALUES (2, 'SVR2931');
@@ -9,4 +11,6 @@ INSERT INTO km_svr_type(id, type) VALUES (4, 'SVR2831');
 INSERT INTO km_svr_type(id, type) VALUES (5, 'SVR2916');
 
 INSERT INTO km_svr_type(id, type) VALUES (6, 'SVR2930');
+
+SET IDENTITY_INSERT km_vrs_name off;
 
